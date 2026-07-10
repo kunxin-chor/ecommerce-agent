17d767d3ffca1933ebc64ab1ea4984ddb2a409a5
ckx <ckx@somewhere.com>
2026-07-10 14:09:17 +0800

    Add more functions to the orderServices and orderData layers for admin

--- admin/services/orderServices.js
+++ admin/services/orderServices.js
@@ -4,4 +4,26 @@ async function listOrders() {
   return await orderData.listOrders();
 }
 
-module.exports = { listOrders };
+async function getCompletedOrdersByDateRange(startDate, endDate) {
+  return await orderData.getCompletedOrdersByDateRange(startDate, endDate);
+}
+
+async function getCompletedOrdersByDateRangeForProduct(startDate, endDate, productId) {
+  return await orderData.getCompletedOrdersByDateRangeForProduct(startDate, endDate, productId);
+}
+
+async function tabulateSales(startDate, endDate) {
+  return await orderData.tabulateSales(startDate, endDate);
+}
+
+async function getLowStockProducts(threshold) {
+  return await orderData.getLowStockProducts(threshold);
+}
+
+module.exports = {
+  listOrders,
+  getCompletedOrdersByDateRange,
+  getCompletedOrdersByDateRangeForProduct,
+  tabulateSales,
+  getLowStockProducts
+};
